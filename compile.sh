a4925f2b15fce141a4806d58052840fb9a316031
AMV_Ph34r <AMVPh34r@gmail.com>
2015-03-12 13:04:47 -0400

    Fix compile script for linux

--- compile.sh
+++ compile.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
-gcc -o mousejiggle.x86_64 mousejiggle.c -lX11
+#!/bin/bash
+SRC_DIR="src"
+BIN_DIR="bin"
+
+if [[ ! -d "${SRC_DIR}" ]]; then
+    echo "Error: Source directory does not exist"
+    exit 1
+fi
+
+if [[ ! -d "${BIN_DIR}" ]]; then
+    echo "Creating directory ${BIN_DIR}/"
+    mkdir "${BIN_DIR}"
+fi
+
+gcc -o "${BIN_DIR}/mousejiggle.x86_64" "${SRC_DIR}/mousejiggle.c" -lX11
